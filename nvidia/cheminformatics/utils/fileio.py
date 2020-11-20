c960778070c7df54f00c8e501a2b2f90be94ed6d
Michelle Gill <mgill@nvidia.com>
2020-11-20 18:00:09 +0000

    Num CPU/GPU set by user and logging

---
+++ nvidia/cheminformatics/utils/fileio.py
@@ -0,0 +1,38 @@
+#!/opt/conda/envs/rapids/bin/python3
+#
+# Copyright (c) 2020, NVIDIA CORPORATION.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+BENCHMARK_FILE = './benchmark.csv'
+
+
+def initialize_logfile(benchmark_file=BENCHMARK_FILE):
+    """Initialize benchmark file with header if needed"""
+
+    if not os.path.exists(benchmark_file):
+        with open(benchmark_file, 'w') as fh:
+            fh.write('date,benchmark_type,step,time(hh:mm:ss.ms),n_cpu,n_gpu\n')
+
+
+def log_results(date, benchmark_type, step, time, n_cpu=0, n_gpu=0, benchmark_file=BENCHMARK_FILE):
+    """Log benchmark results to a file"""
+
+    out_list = [date, benchmark_type, step, time, n_cpu, n_gpu]
+    out_fmt = ','.join(['{}'] * len(out_list)) + '\n'
+
+    with open(BENCHMARK_FILE, 'a') as fh:
+        out_string = out_fmt.format(*out_list)
+        fh.write(out_string)

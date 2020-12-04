03f7bc980ef85095f11066d9ebef1b21810f416e
Michelle Gill <michelle@michellelynngill.com>
2020-12-04 17:21:45 +0000

    Add wrapper for embedding model

--- nvidia/cheminformatics/fingerprint.py
+++ nvidia/cheminformatics/fingerprint.py
@@ -3,13 +3,14 @@ from abc import ABC
 from enum import Enum
 from rdkit import Chem
 from rdkit.Chem import AllChem
-
+from cddd.inference import InferenceModel
 
 logger = logging.getLogger(__name__)
 
 class TransformationDefaults(Enum):
     morgan_fingerprint = {'radius':2, 'nBits':512}
     MorganFingerprint = {'radius':2, 'nBits':512}
+    Embeddings = {}
 
 
 class BaseTransformation(ABC):
@@ -45,25 +46,28 @@ class MorganFingerprint(BaseTransformation):
         return self.kwargs['nBits']
         
 
+class Embeddings(BaseTransformation):
+    MODEL_DIR = '/workspace/cddd/default_model'
+
+    def __init__(self, use_gpu=True, cpu_threads=5, **kwargs):
+        self.name = __class__.__name__.split('.')[-1]
+        self.kwargs = TransformationDefaults[self.name].value
+        self.kwargs.update(kwargs)
+        self.func = InferenceModel(self.MODEL_DIR, use_gpu=use_gpu, cpu_threads=cpu_threads)
+
+    def transform(self, data):
+        if isinstance(data, str):
+            data = [data]
+        return self.func.seq_to_emb(data)
+
+    def __len__(self):
+        return self.func.hparams.emb_size
+
+
 ### DEPRECATED ###
 def morgan_fingerprint(smiles, radius=2, nBits=512):
     m = Chem.MolFromSmiles(smiles)
     fp = AllChem.GetMorganFingerprintAsBitVect(m, radius=radius, nBits=nBits)
 
     return ', '.join(list(fp.ToBitString()))
-
-
-def get_transformation_function_kwargs(transformation_function=morgan_fingerprint, enum_class=TransformationDefaults, **transformation_kwargs):
-    """Get arguments for transformation function"""
-
-    if not isinstance(transformation_function, str):
-       transformation_function = transformation_function.__name__
-
-    tf_default_dict = enum_class[transformation_function].value
-    for key in tf_default_dict:
-        transformation_kwargs[key] = tf_default_dict[key]
-
-    # TODO functools.partial does not seem compatible with Dask. Is there a workaround?
-    # return partial(transformation_function, **transformation_kwargs)
-    return transformation_kwargs
 

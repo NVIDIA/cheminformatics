4285f602cb07e09118ca613d34f2bdfb445f27c8
Michelle Gill <michelle@michellelynngill.com>
2020-12-18 22:17:14 +0000

    spearman r calculation

--- nvidia/cheminformatics/workflow.py
+++ nvidia/cheminformatics/workflow.py
@@ -15,7 +15,12 @@
 # limitations under the License.
 
 import logging
+from math import tan
+import numpy
+
 from nvidia.cheminformatics.utils.fileio import log_results
+from nvidia.cheminformatics.utils.metrics import batched_silhouette_scores, spearman_rho
+from nvidia.cheminformatics.utils.distance import tanimoto_calculate
 
 from datetime import datetime
 
@@ -26,11 +31,11 @@ from cuml.manifold import UMAP as cuUMAP
 from cuml.dask.decomposition import PCA as cuDaskPCA
 from cuml.dask.cluster import KMeans as cuDaskKMeans
 from cuml.dask.manifold import UMAP as cuDaskUMAP
+from cuml.metrics import pairwise_distances
 
 import sklearn.cluster
 import sklearn.decomposition
 import umap
-
 
 logger = logging.getLogger(__name__)
 
@@ -70,10 +75,12 @@ class CpuWorkflow:
         # kmeans_float = sklearn.cluster.KMeans(n_clusters=self.n_clusters)
         kmeans_float = dask_ml.cluster.KMeans(n_clusters=self.n_clusters)
         kmeans_float.fit(df_fingerprints)
-
+        kmeans_labels = kmeans_float.labels_
         runtime = datetime.now() - task_start_time
-        logger.info('### Runtime Kmeans time (hh:mm:ss.ms) {}'.format(runtime))
-        log_results(task_start_time, 'cpu', 'kmeans', runtime, n_cpu=n_cpu)
+
+        silhouette_score = batched_silhouette_scores(df_fingerprints, kmeans_labels, on_gpu=False)
+        logger.info('### Runtime Kmeans time (hh:mm:ss.ms) {} and silhouette score {}'.format(runtime, silhouette_score))
+        log_results(task_start_time, 'gpu', 'kmeans', runtime, n_gpu=n_gpu, metric_name='silhouette_score', metric_value=silhouette_score)
 
         logger.info('UMAP...')
         task_start_time = datetime.now()
@@ -84,8 +91,9 @@ class CpuWorkflow:
         mol_df = mol_df.compute()
         mol_df['x'] = Xt[:, 0]
         mol_df['y'] = Xt[:, 1]
-        mol_df['cluster'] = kmeans_float.labels_
+        mol_df['cluster'] = kmeans_labels
         runtime = datetime.now() - task_start_time
+        
         logger.info('### Runtime UMAP time (hh:mm:ss.ms) {}'.format(runtime))
         log_results(task_start_time, 'cpu', 'umap', runtime, n_cpu=n_cpu)
 
@@ -145,10 +153,12 @@ class GpuWorkflow:
         kmeans_cuml = cuDaskKMeans(client=self.client,
                                    n_clusters=self.n_clusters)
         kmeans_cuml.fit(gdf)
-        kmeans_labels = kmeans_cuml.predict(gdf)
+        kmeans_labels = kmeans_cuml.labels_
         runtime = datetime.now() - task_start_time
-        logger.info('### Runtime Kmeans time (hh:mm:ss.ms) {}'.format(runtime))
-        log_results(task_start_time, 'gpu', 'kmeans', runtime, n_gpu=n_gpu)
+
+        silhouette_score = batched_silhouette_scores(gdf, kmeans_labels, on_gpu=True)
+        logger.info('### Runtime Kmeans time (hh:mm:ss.ms) {} and silhouette score {}'.format(runtime, silhouette_score))
+        log_results(task_start_time, 'gpu', 'kmeans', runtime, n_gpu=n_gpu, metric_name='silhouette_score', metric_value=silhouette_score)
 
         task_start_time = datetime.now()
         local_model = cuUMAP()
@@ -163,8 +173,15 @@ class GpuWorkflow:
                                 client=self.client)
         Xt = umap_model.transform(gdf)
         runtime = datetime.now() - task_start_time
-        logger.info('### Runtime UMAP time (hh:mm:ss.ms) {}'.format(runtime))
-        log_results(task_start_time, 'gpu', 'umap', runtime, n_gpu=n_gpu)
+
+        n_indexes = 5000
+        indexes = numpy.random.choice(numpy.array(range(X_train.shape[0])), size=n_indexes, replace=False)
+        dist_array_tani = tanimoto_calculate(X_train.iloc[indexes])
+        dist_array_eucl = pairwise_distances(Xt.iloc[indexes])
+        spearman_mean = spearman_rho(dist_array_tani, dist_array_eucl).mean()
+
+        logger.info('### Runtime UMAP time (hh:mm:ss.ms) {} with {} of {}'.format(runtime, 'spearman_rho', spearman_mean))
+        log_results(task_start_time, 'gpu', 'umap', runtime, n_gpu=n_gpu, metric_name='spearman_rho', metric_value=spearman_mean)
 
         # Add back the column required for plotting and to correlating data
         # between re-clustering

465c0570562a451de5796a167049cf7c78af6b5f
Rahul Mohan <brxownboy@gmail.com>
2021-01-21 14:38:05 -0500

    Added new GPU workflow

--- nvidia/cheminformatics/workflow.py
+++ nvidia/cheminformatics/workflow.py
@@ -35,6 +35,7 @@ from cuml.dask.decomposition import PCA as cuDaskPCA
 from cuml.dask.cluster import KMeans as cuDaskKMeans
 from cuml.dask.manifold import UMAP as cuDaskUMAP
 from cuml.metrics import pairwise_distances
+from cuml import SparseRandomProjection, KMeans
 
 import sklearn.cluster
 import sklearn.decomposition
@@ -49,13 +50,11 @@ class CpuWorkflow:
                  client,
                  n_molecules,
                  pca_comps=64,
-                 n_clusters=7,
-                 benchmark_file='./benchmark.csv'):
+                 n_clusters=7,):
         self.client = client
         self.n_molecules = n_molecules
         self.pca_comps = pca_comps
         self.n_clusters = n_clusters
-        self.benchmark_file = benchmark_file
 
     def execute(self, mol_df):
         logger.info("Executing CPU workflow...")
@@ -73,7 +72,7 @@ class CpuWorkflow:
             runtime = datetime.now() - task_start_time
             logger.info(
                 '### Runtime PCA time (hh:mm:ss.ms) {}'.format(runtime))
-            log_results(task_start_time, 'cpu', 'pca', runtime, n_molecules=self.n_molecules, n_workers=n_cpu, metric_name='', metric_value='', benchmark_file=self.benchmark_file)
+            log_results(task_start_time, 'cpu', 'pca', runtime, n_molecules=self.n_molecules, n_workers=n_cpu, metric_name='', metric_value='')
         else:
             df_fingerprints = mol_df.copy()
 
@@ -82,12 +81,11 @@ class CpuWorkflow:
         # kmeans_float = sklearn.cluster.KMeans(n_clusters=self.n_clusters)
         kmeans_float = dask_KMeans(n_clusters=self.n_clusters)
         kmeans_float.fit(df_fingerprints)
-        kmeans_labels = kmeans_float.predict(df_fingerprints)
-        runtime = datetime.now() - task_start_time
 
-        silhouette_score = batched_silhouette_scores(df_fingerprints, kmeans_labels, on_gpu=False)
+        #silhouette_score = batched_silhouette_scores(df_fingerprints, kmeans_labels, on_gpu=False)
+        silhouette_score = 0.0
         logger.info('### Runtime Kmeans time (hh:mm:ss.ms) {} and silhouette score {}'.format(runtime, silhouette_score))
-        log_results(task_start_time, 'cpu', 'kmeans', runtime, n_molecules=self.n_molecules, n_workers=n_cpu, metric_name='silhouette_score', metric_value=silhouette_score, benchmark_file=self.benchmark_file)
+        log_results(task_start_time, 'cpu', 'kmeans', runtime, n_molecules=self.n_molecules, n_workers=n_cpu, metric_name='silhouette_score', metric_value=silhouette_score)
 
         logger.info('UMAP...')
         task_start_time = datetime.now()
@@ -102,7 +100,7 @@ class CpuWorkflow:
         runtime = datetime.now() - task_start_time
         
         logger.info('### Runtime UMAP time (hh:mm:ss.ms) {}'.format(runtime))
-        log_results(task_start_time, 'cpu', 'umap', runtime, n_molecules=self.n_molecules, n_workers=n_cpu, metric_name='', metric_value='', benchmark_file=self.benchmark_file)
+        log_results(task_start_time, 'cpu', 'umap', runtime, n_molecules=self.n_molecules, n_workers=n_cpu, metric_name='', metric_value='')
 
         return mol_df
 
@@ -113,13 +111,11 @@ class GpuWorkflow:
                  client,
                  n_molecules,
                  pca_comps=64,
-                 n_clusters=7,
-                 benchmark_file='./benchmark.csv'):
+                 n_clusters=7,):
         self.client = client
         self.n_molecules = n_molecules
         self.pca_comps = pca_comps
         self.n_clusters = n_clusters
-        self.benchmark_file = benchmark_file
 
     def re_cluster(self, gdf,
                    new_figerprints=None,
@@ -166,10 +162,11 @@ class GpuWorkflow:
         kmeans_cuml.fit(gdf)
         kmeans_labels = kmeans_cuml.predict(gdf)
         runtime = datetime.now() - task_start_time
-        
-        silhouette_score = batched_silhouette_scores(gdf, kmeans_labels, on_gpu=True)
+
+        # silhouette_score = batched_silhouette_scores(gdf, kmeans_labels, on_gpu=True)
+        silhouette_score = 0.0
         logger.info('### Runtime Kmeans time (hh:mm:ss.ms) {} and silhouette score {}'.format(runtime, silhouette_score))
-        log_results(task_start_time, 'gpu', 'kmeans', runtime, n_molecules=self.n_molecules, n_workers=n_gpu, metric_name='silhouette_score', metric_value=silhouette_score, benchmark_file=self.benchmark_file)
+        log_results(task_start_time, 'gpu', 'kmeans', runtime, n_molecules=self.n_molecules, n_workers=n_gpu, metric_name='silhouette_score', metric_value=silhouette_score)
 
         task_start_time = datetime.now()
         local_model = cuUMAP()
@@ -195,7 +192,7 @@ class GpuWorkflow:
         spearman_mean = spearman_rho(dist_array_tani, dist_array_eucl).mean()
 
         logger.info('### Runtime UMAP time (hh:mm:ss.ms) {} with {} of {}'.format(runtime, 'spearman_rho', spearman_mean))
-        log_results(task_start_time, 'gpu', 'umap', runtime, n_molecules=self.n_molecules, n_workers=n_gpu, metric_name='spearman_rho', metric_value=spearman_mean, benchmark_file=self.benchmark_file)
+        log_results(task_start_time, 'gpu', 'umap', runtime, n_molecules=self.n_molecules, n_workers=n_gpu, metric_name='spearman_rho', metric_value=spearman_mean)
 
         # Add back the column required for plotting and to correlating data
         # between re-clustering
@@ -221,9 +218,103 @@ class GpuWorkflow:
             runtime = datetime.now() - task_start_time
             logger.info(
                 '### Runtime PCA time (hh:mm:ss.ms) {}'.format(runtime))
-            log_results(task_start_time, 'gpu', 'pca', runtime, n_molecules=self.n_molecules, n_workers=n_gpu, metric_name='', metric_value='', benchmark_file=self.benchmark_file)
+            log_results(task_start_time, 'gpu', 'pca', runtime, n_molecules=self.n_molecules, n_workers=n_gpu, metric_name='', metric_value='')
         else:
             df_fingerprints = mol_df.copy()
 
         mol_df = self.re_cluster(df_fingerprints)
+        return mol_df
+
+class GpuWorkflowRandomProjection:
+
+    def __init__(self,
+                 client,
+                 n_molecules,
+                 n_clusters=8):
+
+        self.client = client
+        self.n_molecules = n_molecules
+        self.n_clusters = n_clusters
+        self.embedding = SparseRandomProjection(n_components=2)
+
+    def re_cluster(self, mol_df, gdf,
+                   new_figerprints=None,
+                   new_chembl_ids=None,
+                   n_clusters = None):
+
+        if n_clusters is not None:
+            self.n_clusters = n_clusters
+
+        n_gpu = len(self.client.cluster.workers)
+        logger.info('WORKERS %d' % n_gpu)
+        # Before reclustering remove all columns that may interfere
+        if 'id' in gdf.columns:
+            gdf = gdf.drop(['x', 'y', 'cluster', 'id'], axis=1)
+
+        if 'filter_col' in gdf.columns:
+            gdf = gdf.drop(['filter_col'], axis=1)
+
+        if new_figerprints is not None and new_chembl_ids is not None:
+            # Add new figerprints and chEmblIds before reclustering
+            mol_df.append(new_figerprints, ignore_index=True)
+            new_figerprints = self.embedding.transform(new_figerprints)
+
+            fp_df = cudf.DataFrame(
+                new_figerprints,
+                index=[idx for idx in range(self.orig_df.shape[0],
+                                            self.orig_df.shape[0] + len(new_figerprints))],
+                columns=gdf.columns)
+
+            gdf = gdf.append(fp_df, ignore_index=True)
+            # Update original dataframe for it to work on reload
+            fp_df['id'] = fp_df.index
+            self.orig_df = self.orig_df.append(fp_df, ignore_index=True)
+            chembl_ids = chembl_ids.append(
+                cudf.Series(new_chembl_ids), ignore_index=True)
+            ids = ids.append(fp_df['id'], ignore_index=True), 'id', 'chembl_id'
+            self.chembl_ids.extend(new_chembl_ids)
+
+            del fp_df
+
+        task_start_time = datetime.now()
+        kmeans_cuml = KMeans(n_clusters=self.n_clusters)
+        kmeans_cuml.fit(gdf)
+        kmeans_labels = kmeans_cuml.predict(gdf)
+        runtime = datetime.now() - task_start_time
+
+        # silhouette_score = batched_silhouette_scores(gdf, kmeans_labels, on_gpu=True)
+        silhouette_score = 0.0
+        logger.info('### Runtime Kmeans time (hh:mm:ss.ms) {} and silhouette score {}'.format(runtime, silhouette_score))
+        log_results(task_start_time, 'gpu', 'kmeans', runtime, n_molecules=self.n_molecules, n_workers=n_gpu, metric_name='silhouette_score', metric_value=silhouette_score)
+
+        # Sample to calculate spearman's rho
+        n_indexes = 5000
+        indexes = numpy.random.choice(numpy.array(range(X_train.shape[0])), size=n_indexes, replace=False)
+        X_train_sample = cupy.fromDlpack(mol_df.to_dlpack())[indexes]
+        Xt_sample = gdf[indexes]
+        dist_array_tani = tanimoto_calculate(X_train_sample, calc_distance=True)
+        dist_array_eucl = pairwise_distances(Xt_sample)
+        spearman_mean = spearman_rho(dist_array_tani, dist_array_eucl).mean()
+
+        gdf['x'] = gdf[:, 0]
+        gdf['y'] = gdf[:, 1]
+        gdf['cluster'] = kmeans_labels
+        gdf['id'] = mol_df.index
+
+        return gdf
+
+    def execute(self, mol_df):
+        logger.info("Executing GPU workflow...")
+        n_gpu = len(self.client.cluster.workers)
+        mol_df = dask_cudf.from_dask_dataframe(mol_df)
+        mol_df = mol_df.persist()
+
+        logger.info('SparseRandomProjection...')
+        task_start_time = datetime.now()
+        df_fingerprints = self.embedding.fit_transform(cupy.fromDlpack(mol_df.compute().to_dlpack()))
+        runtime = datetime.now() - task_start_time
+
+        logger.info('### Runtime Sparse Random Projection time (hh:mm:ss.ms) {}'.format(runtime))
+        log_results(task_start_time, 'gpu', 'random_proj', runtime, n_molecules=self.n_molecules, n_workers=n_gpu, metric_name='', metric_value='')
+        mol_df = self.re_cluster(mol_df, df_fingerprints)
         return mol_df

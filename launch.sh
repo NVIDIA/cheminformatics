64175ea45dc2b5857afd725b37d91021073bbc6a
Rajesh K Ilango <rilango@gmail.com>
2021-02-18 14:41:17 -0800

    Bugfix: (Review comment) Change container location
    Includes an ansible script to setup a host with required driver, toolkit and docker engine installed.

--- launch.sh
+++ launch.sh
@@ -105,7 +105,7 @@ fi
 #
 ###############################################################################
 
-CONT=${CONT:=cheminf-clustering}
+CONT=${CONT:=nvcr.io/nvidia/clara/cheminformatics_demo:200929-0.0.1}
 JUPYTER_PORT=${JUPYTER_PORT:-9000}
 PLOTLY_PORT=${PLOTLY_PORT:-5000}
 DASK_PORT=${DASK_PORT:-9001}

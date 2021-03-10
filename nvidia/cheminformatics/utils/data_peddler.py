33535ea34f7e7da20a614dc1a0c5a4bb5a2771e7
Rajesh Ilango <rilango@nvidia.com>
2021-03-09 16:31:45 -0800

    Bugfix: Error downloading model for the first time...
    The code meant to clean the model directory fails when there is no directory to
    cleanup. New env. never got tested.

--- nvidia/cheminformatics/utils/data_peddler.py
+++ nvidia/cheminformatics/utils/data_peddler.py
@@ -19,7 +19,7 @@ def download_cddd_models(target_dir=CDDD_DEFAULT_MODLE_LOC):
         logger.warning('Directory already exists. To re-download please delete %s', target_dir)
         return os.path.join(target_dir, 'default_model')
     else:
-        shutil.rmtree(os.path.join(target_dir, 'default_model'))
+        shutil.rmtree(os.path.join(target_dir, 'default_model'), ignore_errors=True)
 
     download_script = '/opt/cddd/download_default_model.sh'
     if not os.path.exists(download_script):
@@ -28,7 +28,9 @@ def download_cddd_models(target_dir=CDDD_DEFAULT_MODLE_LOC):
              'wget --quiet -O %s https://raw.githubusercontent.com/jrwnter/cddd/master/download_default_model.sh && chmod +x %s' % (download_script, download_script)])
 
     run(['bash', '-c',
-         'mkdir -p %s && cd %s; %s' % (target_dir, target_dir, download_script)],
+         'mkdir -p %s && cd %s; %s' % (os.path.join(target_dir, 'default_model'),
+                                       target_dir,
+                                       download_script)],
         check=True)
 
     return os.path.join(target_dir, 'default_model')

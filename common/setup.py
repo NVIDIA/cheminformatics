e5c141f1775f019a6680291f2fe4617ac562388f
Rajesh Ilango <rilango@nvidia.com>
2021-04-26 21:15:12 -0700

    Initial refactor for supporting more than one container to work with the application.

---
+++ common/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cuchem-commons',
+    version='0.1',
+    packages=find_packages(),
+    description='Common components used in Nvidia ChemInformatics'
+)

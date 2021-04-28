54d19062ce582da594f2430af690bdd879e09241
Rajesh Ilango <rilango@nvidia.com>
2021-04-28 14:06:29 -0700

    Move grpc code into megamolbart.

--- megamolbart/megamolbart/service.py
+++ megamolbart/megamolbart/service.py
@@ -10,8 +10,8 @@ logger = logging.getLogger(__name__)
 class GenerativeSampler(generativesampler_pb2_grpc.GenerativeSampler):
 
     def __init__(self, *args, **kwargs):
-        self.cddd = Cddd()
-        self.molbart = MolBART()
+        pass
+
 
     def FindSimilars(self, spec, context):
         # Code for using generative model

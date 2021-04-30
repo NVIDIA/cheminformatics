6695d1965078f7e05bf77bfd68378eccdd6be46e
Michelle Gill <mgill@nvidia.com>
2021-04-30 12:10:11 -0400

    working benchmark before merge

--- megamolbart/megamolbart/inference.py
+++ megamolbart/megamolbart/inference.py
@@ -32,6 +32,7 @@ DEFAULT_MAX_SEQ_LEN = 512
 DEFAULT_VOCAB_PATH = '/models/megamolbart/bart_vocab.txt'
 CHECKPOINTS_DIR = '/models/megamolbart/checkpoints'
 
+
 @add_jitter.register(torch.Tensor)
 def _(embedding, radius, cnt):
     permuted_emb = embedding.permute(1, 0, 2)
@@ -40,10 +41,30 @@ def _(embedding, radius, cnt):
     return (noise + permuted_emb).permute(1, 0, 2)
 
 
+def clean_smiles_list(smiles_list, standardize=True):
+    """Ensure SMILES are valid and unique. Optionally standardize them."""
+
+    smiles_clean_list = []
+    for smiles in smiles_list:
+        mol = Chem.MolFromSmiles(smiles, sanitize=standardize)
+        if mol:
+            sanitized_smiles = Chem.MolToSmiles(mol)
+            if sanitized_smiles not in smiles_clean_list:
+                smiles_clean_list.append(sanitized_smiles)
+
+    if len(smiles_clean_list) == 0:
+        smiles_clean_list = [np.NaN]
+    return smiles_clean_list
+
+
 class MegaMolBART(BaseGenerativeWorkflow):
 
     def __init__(self) -> None:
         super().__init__()
+
+        self.device = 'cuda' # Megatron arg loading seems to only work with GPU
+        self.radius_scale = 0.0001 # TODO adjust this once model is trained
+        self.max_seq_len = DEFAULT_MAX_SEQ_LEN
 
         args = {
                 'num_layers': 4,
@@ -54,10 +75,6 @@ class MegaMolBART(BaseGenerativeWorkflow):
                 'vocab_file': DEFAULT_VOCAB_PATH,
                 'load': CHECKPOINTS_DIR # this is the checkpoint path
             }
-
-        self.device = 'cuda' # Megatron arg loading seems to only work with GPU
-        max_seq_len = DEFAULT_MAX_SEQ_LEN
-        self.radius_scale = 0.0001 # TODO adjust this once model is trained
 
         torch.set_grad_enabled(False) # Testing this instead of `with torch.no_grad():` context since it doesn't exit
         initialize_megatron(args_defaults=args)
@@ -165,10 +182,12 @@ class MegaMolBART(BaseGenerativeWorkflow):
             # TODO: add back sanitization and validity checking once model is trained
             logger.warn('WARNING: MOLECULE VALIDATION AND SANITIZATION CURRENTLY DISABLED')
             for smiles in mol_strs:
-            #     mol = Chem.MolFromSmiles(smiles, sanitize=sanitize)
-            #     if (mol is not None) and (smiles not in smiles_interp_list):
-            #         smiles_interp_list.append(mol)
-            #         break
+                # mol = Chem.MolFromSmiles(smiles, sanitize=sanitize)
+                # if mol:
+                #     sanitized_smiles = Chem.MolToSmiles(mol)
+                #     if sanitized_smiles not in smiles_interp_list:
+                #         smiles_interp_list.append(sanitized_smiles)
+                #         break
                 smiles_interp_list.append(smiles)
 
         return smiles_interp_list
@@ -274,8 +293,7 @@ class MegaMolBART(BaseGenerativeWorkflow):
             interpolated_mol.append(smiles[idx + 1])
 
             interp_df = pd.DataFrame({'SMILES': interpolated_mol,
-                                      'Generated': [True for i in range(len(interpolated_mol))]},
-                                      )
+                                      'Generated': [True for i in range(len(interpolated_mol))]})
 
             inv_transform_funct = partial(self.inverse_transform, mem_pad_mask=combined_mask)
 

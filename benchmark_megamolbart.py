6b568ee860a2368e8a79e16e3f9b6a2a9d28b7c4
Michelle Gill <mgill@nvidia.com>
2021-05-01 18:26:41 -0400

    Working command line benchmark

--- benchmark_megamolbart.py
+++ benchmark_megamolbart.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+import os
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 import torch
 from rdkit import Chem
 
@@ -9,7 +11,11 @@ import sys
 sys.path.insert(0, "/workspace/megamolbart")
 from megamolbart.inference import MegaMolBART
 
-# TODO ensure ouput smiles != input
+
+BENCHMARK_DRUGS_PATH = '/workspace/cuchem/tests/data/benchmark_approved_drugs.csv'
+BENCHMARK_OUTPUT_PATH = '/workspace/megamolbart/benchmark'
+
+
 def validate_smiles_list(smiles_list):
     """Ensure SMILES are valid and sanitized, otherwise fill with NaN."""
     smiles_clean_list = []
@@ -32,28 +38,62 @@ def do_sampling(data, func, num_samples, radius_list, radius_scale):
         
         for smiles in data:
             smiles_df = func(smiles, num_samples, radius=simulated_radius)
+
             smiles_df = smiles_df[smiles_df['Generated']]
+            # All molecules are valid and not the same as input
             smiles_df = pd.DataFrame({'OUTPUT': validate_smiles_list(smiles_df['SMILES'].tolist())})
+            if not isinstance(smiles, list):
+                smiles = [smiles]
+            mask = smiles_df['OUTPUT'].isin(smiles).pipe(np.invert)
+            smiles_df = smiles_df[mask]
+            smiles_df['INPUT'] = ','.join(smiles)
             smiles_df['RADIUS'] = radius
-            if isinstance(smiles, list):
-                smiles = ','.join(smiles)
-            smiles_df['INPUT'] = smiles
             smiles_results.append(smiles_df)
 
     smiles_results = pd.concat(smiles_results, axis=0).reset_index(drop=True)[['RADIUS', 'INPUT', 'OUTPUT']]
     return smiles_results
 
 
+def calc_statistics(df, level):
+    """Calculate validity and uniqueness statistics per molecule or per radius / sampling type"""
+    def _percent_valid(df):
+        return len(df.dropna()) / float(len(df))
+    
+    def _percent_unique(df):
+        return len(set(df.dropna().tolist())) / float(len(df))
+    
+    results = df.groupby(level=level).agg([_percent_valid, _percent_unique])
+    results.columns = ['percent_valid', 'percent_unique']
+    return results
+
+
+def plot_results(overall):
+    """Plot the overall data statistics"""
+    fig, axList = plt.subplots(ncols=2, nrows=1)
+    fig.set_size_inches(10, 6)
+    for ax, sample, kind in zip(axList, ['interp', 'single'], ['bar', 'line']):
+        plt_data = overall.loc[sample]
+        plt_data.plot(kind=kind, ax=ax)
+        ax.set(title=f'Latent Space Sampling Type: {sample.title()}')    
+        if sample == 'single':
+            ax.set(xlabel='Radius', ylabel='Percent')
+        else:
+            ax.set(xlabel='', ylabel='Percent')
+            ax.set_xticklabels([])
+    fig.savefig(os.path.join(BENCHMARK_OUTPUT_PATH, 'overall.png'))
+
+
 if __name__ == '__main__':
 
-    num_molecules = 3
-    num_samples = 4
-    radius_list = [0.0001, 0.001]    
-    data = pd.read_csv('/workspace/cuchem/tests/data/benchmark_approved_drugs.csv')
+    # num_molecules = 10
+    # num_samples = 10
+    # radius_list = [0.00001, 0.00005, 0.0001, 0.0005, 0.001]  
+    num_molecules = 2
+    num_samples = 5
+    radius_list = [0.00001, 0.00005]    
+    data = pd.read_csv(BENCHMARK_DRUGS_PATH)
 
     with torch.no_grad():
-        
-
         wf = MegaMolBART()
         master_df = list()
         for sample_type in ['single', 'interp']:
@@ -62,43 +102,30 @@ if __name__ == '__main__':
             if sample_type == 'single':
                 sampled_data = data['canonical_smiles'].sample(n=num_molecules, replace=False, random_state=0).tolist()
                 func = wf.find_similars_smiles
+                smiles_results = do_sampling(sampled_data, func, num_samples, radius_list, wf.radius_scale)
             else:
+                # Sample two at a time -- must ensure seed is different each time
                 sampled_data = [data['canonical_smiles'].sample(n=2, replace=False, random_state=i).tolist() for i in range(num_molecules)]
                 func = wf.interpolate_from_smiles
-                
-            smiles_results = do_sampling(sampled_data, func, num_samples, radius_list, wf.radius_scale)
+                # radius not used for sampling two at a time -- enter dummy here
+                smiles_results = do_sampling(sampled_data, func, num_samples, [1.0], 1.0)
+                #smiles_results['RADIUS'] = np.NaN
+
             smiles_results['SAMPLE'] = sample_type
             master_df.append(smiles_results)
-        master_df = pd.concat(master_df, axis=0).set_index(['SAMPLE', 'RADIUS', 'INPUT'])
-
     
-    def _percent_valid(df):
-        return len(df['OUTPUT'].dropna()) / float(len(df['OUTPUT']))
+    indexes = ['SAMPLE', 'RADIUS', 'INPUT']
+    master_df = pd.concat(master_df, axis=0).set_index(indexes)
+    results = calc_statistics(master_df, indexes)
+    overall = calc_statistics(master_df, indexes[:-1])
+
+    with open(os.path.join(BENCHMARK_OUTPUT_PATH, 'results.md'), 'w') as fh:
+        results.reset_index().to_markdown(fh)
     
-    def _percent_unique(df):
-        return len(set(df['OUTPUT'].dropna().tolist())) / float(len(df['OUTPUT']))
+    with open(os.path.join(BENCHMARK_OUTPUT_PATH, 'overall.md'), 'w') as fh:
+        overall.reset_index().to_markdown(fh)
 
-    # Aggregate statistics for each molecule
-    percent_valid_per_query = master_df.groupby(level=[0,1,2]).apply(lambda x: _percent_valid(x))
-    percent_valid_per_query.name = 'percent_valid'
-    percent_unique_per_query = master_df.groupby(level=[0,1,2]).apply(lambda x: _percent_unique(x))
-    percent_unique_per_query.name = 'percent_unique'
-    results = pd.merge(percent_valid_per_query, percent_unique_per_query, left_index=True, right_index=True)
+    plot_results(overall)
+    
+    
 
-    # Aggregate statistics for entire dataset
-    percent_valid = master_df.groupby(level=[0,1]).apply(lambda x: _percent_valid(x))
-    percent_valid.name = 'percent_valid'
-    percent_valid = percent_valid.to_frame().reset_index()
-    percent_valid['INPUT'] = 'TOTAL'
-    percent_valid = percent_valid.set_index(['SAMPLE', 'RADIUS', 'INPUT'])
-
-    percent_unique = master_df.groupby(level=[0,1]).apply(lambda x: _percent_unique(x))
-    percent_unique.name = 'percent_unique'
-    percent_unique = percent_unique.to_frame().reset_index()
-    percent_unique['INPUT'] = 'TOTAL'
-    percent_unique = percent_unique.set_index(['SAMPLE', 'RADIUS', 'INPUT'])
-    overall = pd.merge(percent_valid, percent_unique, left_index=True, right_index=True)
-    results = pd.concat([results, overall], axis=0)
-
-
-        
---
+++ megamolbart/benchmark/results.md
@@ -0,0 +1,8 @@
+|    | SAMPLE   |   RADIUS | INPUT                                                                                        |   percent_valid |   percent_unique |
+|---:|:---------|---------:|:---------------------------------------------------------------------------------------------|----------------:|-----------------:|
+|  0 | interp   |    1     | CCCCS(=O)(=O)N[C@@H](Cc1ccc(OCCCCC2CCNCC2)cc1)C(=O)O,CCCN(CCC)C(=O)C(CCC(=O)O)NC(=O)c1ccccc1 |               1 |              1   |
+|  1 | interp   |    1     | CCNC(C)Cc1cccc(C(F)(F)F)c1.Cl,NS(=O)(=O)c1ccc(N2CCCCS2(=O)=O)cc1                             |               1 |              1   |
+|  2 | single   |    1e-05 | CCCCS(=O)(=O)N[C@@H](Cc1ccc(OCCCCC2CCNCC2)cc1)C(=O)O                                         |               1 |              0.5 |
+|  3 | single   |    1e-05 | CCCN(CCC)C(=O)C(CCC(=O)O)NC(=O)c1ccccc1                                                      |               1 |              0.2 |
+|  4 | single   |    5e-05 | CCCCS(=O)(=O)N[C@@H](Cc1ccc(OCCCCC2CCNCC2)cc1)C(=O)O                                         |               1 |              1   |
+|  5 | single   |    5e-05 | CCCN(CCC)C(=O)C(CCC(=O)O)NC(=O)c1ccccc1                                                      |               1 |              1   |

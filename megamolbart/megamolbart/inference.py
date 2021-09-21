6e3c71c19d5fa8f460b9c9f3de8dade84e0fb8f3
Rajesh K Ilango <rilango@gmail.com>
2021-09-20 18:00:05 -0700

    Benchmarking, Remove NGC, Invalid ChEMBLE id validation

--- megamolbart/megamolbart/inference.py
+++ megamolbart/megamolbart/inference.py
@@ -165,7 +165,7 @@ class MegaMolBART(BaseGenerativeWorkflow):
         torch.cuda.empty_cache()
         return embedding, pad_mask
 
-    def inverse_transform(self, embeddings, mem_pad_mask, k=1, sanitize=False):
+    def inverse_transform(self, embeddings, mem_pad_mask, k=1, sanitize=True):
         mem_pad_mask = mem_pad_mask.clone()
         smiles_interp_list = []
 

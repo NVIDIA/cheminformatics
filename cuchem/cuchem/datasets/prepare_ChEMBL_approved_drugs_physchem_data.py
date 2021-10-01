56eb1b9b42e559158be2f67c045ff39eeaf34167
Michelle Gill <michelle@michellelynngill.com>
2021-10-01 19:01:41 +0000

    Code for pulling data finished

--- cuchem/cuchem/datasets/prepare_ChEMBL_approved_drugs_physchem_data.py
+++ cuchem/cuchem/datasets/prepare_ChEMBL_approved_drugs_physchem_data.py
@@ -23,22 +23,34 @@ import pathlib
 from cuchemcommon.data.helper.chembldata import ChEmblData
 from cuchemcommon.fingerprint import calc_morgan_fingerprints
 
-DATA_BENCHMARK_DIR = os.path.join(pathlib.Path(__file__).absolute().parent.parent.parent,
-                                  'tests', 'data')
+DATA_BENCHMARK_DIR = os.path.join(pathlib.Path(__file__).absolute().parent.parent,
+                                'data')
+columns = ['molregno', 'canonical_smiles', 'max_phase_for_ind'] 
+physchem_columns = ['mw_freebase', 'alogp', 'hba', 'hbd', 'psa', 'rtb', \
+                    'ro3_pass', 'num_ro5_violations', 'cx_logp', 'cx_logd', \
+                    'full_mwt', 'aromatic_rings', 'heavy_atoms', 'qed_weighted', \
+                    'mw_monoisotopic', 'hba_lipinski', 'hbd_lipinski', 'num_lipinski_ro5_violations']
 
 if __name__ == '__main__':
-    results = ChEmblData(fp_type=None).fetch_approved_drugs(with_labels=True)
+    results = ChEmblData(fp_type=None).fetch_approved_drugs_physchem(with_labels=True)
     benchmark_df = pd.DataFrame(results[0], columns=results[1])
+    benchmark_df = benchmark_df[columns + physchem_columns]
+    benchmark_df = benchmark_df.rename(columns={'molregno': 'index'})
+
+    keep_mask = benchmark_df['alogp'].notnull()
+    benchmark_df = benchmark_df[keep_mask]
 
     # TODO: benchmark SMILES have not been explicitly canonicalized with RDKit. Should this be done?
     fp = calc_morgan_fingerprints(benchmark_df)
     fp.columns = fp.columns.astype(np.int64)
-    fp.index = fp.index.astype(np.int64)
     for col in fp.columns:
         fp[col] = fp[col].astype(np.float32)
-
+    fp.index = benchmark_df.index.astype(np.int64)
+    fp.index.rename('index')
+    fp = fp.reset_index()
+    
     # Write results
-    benchmark_df.to_csv(os.path.join(DATA_BENCHMARK_DIR, 'benchmark_approved_drugs.csv'))
-    fp.to_csv(os.path.join(DATA_BENCHMARK_DIR, 'fingerprints_approved_drugs.csv'))
+    benchmark_df.to_csv(os.path.join(DATA_BENCHMARK_DIR, 'benchmark_approved_drugs_physchem.csv'), index=False)
+    fp.to_csv(os.path.join(DATA_BENCHMARK_DIR, 'fingerprints_approved_drugs_physchem.csv'), index=False)
     # fp_hdf5 = cudf.DataFrame(fp)
     # fp_hdf5.to_hdf(os.path.join(DATA_BENCHMARK_DIR, 'filter_00.h5', 'fingerprints', format='table'))

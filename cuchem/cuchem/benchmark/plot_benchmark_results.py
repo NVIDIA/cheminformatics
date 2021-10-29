6cf1727e1ab50b7790e25f7f1c48fd00442ca3bc
Michelle Gill <mgill@nvidia.com>
2021-10-29 22:24:05 +0000

    File cleanup

--- cuchem/cuchem/benchmark/plot_benchmark_results.py
+++ cuchem/cuchem/benchmark/plot_benchmark_results.py
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# TODO cleanup and / or remove as appropriate
+
 import argparse
 import logging
 import os
